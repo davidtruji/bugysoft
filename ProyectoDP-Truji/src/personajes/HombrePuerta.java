c198d0e2c22a7a25cef1447ce4663ee56db40072
truji <truji@truji-SATELLITE-PRO-C50-A-1HQ>
2017-11-06 18:38:33 +0100

    Hombre puerta documentado

--- ProyectoDP-Truji/src/personajes/HombrePuerta.java
+++ ProyectoDP-Truji/src/personajes/HombrePuerta.java
@@ -2,28 +2,62 @@ package personajes;
 
 import estructuras_datos.Arbol;
 
+/**
+ * 
+ * ProyectoDP-Truji
+ * 
+ * @Fichero: HombrePuerta.java
+ * @Autor: David Trujillo Torres
+ * @Fecha: 6 nov. 2017
+ */
 public class HombrePuerta {
 
 	boolean Portal;// TRUE ABIERTO / FALSE CERRADO
 	Arbol<Arma> ContenedorArmas;
 
+	/**
+	 * Constructor por defecto del hombre puerta
+	 */
 	public HombrePuerta() {
 		Portal = false;
 		ContenedorArmas = new Arbol<Arma>();
 	}
 
+	/**
+	 * Get del contenedor de armas del hombre puerta
+	 * 
+	 * @return Arbol de armas
+	 */
 	public Arbol<Arma> getContenedorArmas() {
 		return ContenedorArmas;
 	}
 
+	/**
+	 * Set del contenedor de armas del hombre puerta
+	 * 
+	 * @param contenedorArmas
+	 *            Arbol de armas
+	 */
 	public void setContenedorArmas(Arbol<Arma> contenedorArmas) {
 		ContenedorArmas = contenedorArmas;
 	}
 
+	/**
+	 * Metodo que devulve el estado del portal
+	 * 
+	 * @return True (Abierto) , False (Cerrado)
+	 */
 	public boolean isPortal() {
 		return Portal;
 	}
 
+	/**
+	 * Metodo que comprueba si se cumple la condicion de apertura
+	 * 
+	 * @param altura
+	 *            altura del arbol para abrir el portal
+	 * @return True si se cumple la condicion false en caso contrario
+	 */
 	public boolean condicionApetura(int altura) {
 		boolean portal = false;
 		if (altura > ContenedorArmas.alturaArbol()) {
@@ -34,30 +68,57 @@ public class HombrePuerta {
 
 	}
 
+	/**
+	 * Actualiza el estado del portal comprobando si se cumple la condicion de
+	 * apertura
+	 * 
+	 * @param altura
+	 *            altura del portal para abrirse
+	 */
 	public void ActualizarEstadoPortal(int altura) {
-		
-		if(condicionApetura(altura))
-			Portal=true;
+
+		if (condicionApetura(altura))
+			Portal = true;
 		else
-			Portal=false;
-		
+			Portal = false;
+
 	}
 
+	/**
+	 * Set del portal del hombre puerta
+	 * 
+	 * @param portal
+	 *            estado del portal
+	 */
 	public void setPortal(boolean portal) {
 		Portal = portal;
 	}
 
+	/**
+	 * Metodo que cierra el portal del hombre puerta
+	 */
 	public void cerrar() {
 
 		Portal = false;
 	}
 
+	/**
+	 * Metodo que configura un array de armas en el hombre puerta
+	 * 
+	 * @param armas
+	 *            array de armas
+	 */
 	public void configurar(Arma[] armas) {
 		for (int i = 0; i < armas.length; i++) {
 			ContenedorArmas.insertar(armas[i]);
 		}
 	}
 
+	/**
+	 * Main de la clase Hombre puerta, se usa para pruebas internas de la clase
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Arma[] armasPuerta = { new Arma("CampoEnergia", 5), new Arma("Armadura", 13), new Arma("Anillo", 11),
 				new Arma("Acido", 1), new Arma("Antorcha", 5), new Arma("Bola", 3), new Arma("Baston", 22),
