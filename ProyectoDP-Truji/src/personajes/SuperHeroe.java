2f3901288305a3387dbedad0c9d02cc50e358c34
truji <truji@truji-SATELLITE-PRO-C50-A-1HQ>
2017-11-06 18:42:42 +0100

    Clase superheroe comentada

--- ProyectoDP-Truji/src/personajes/SuperHeroe.java
+++ ProyectoDP-Truji/src/personajes/SuperHeroe.java
@@ -2,40 +2,81 @@ package personajes;
 
 import estructuras_datos.Arbol;
 
+/**
+ * 
+ * ProyectoDP-Truji
+ * 
+ * @Fichero: SuperHeroe.java
+ * @Autor: David Trujillo Torres
+ * @Fecha: 6 nov. 2017
+ */
 public class SuperHeroe extends Personaje {
 
 	private Arbol<Arma> ContenedorArmas;
 
+	/**
+	 * Constructor de super heroe que recibe su nombre y su inicial
+	 * 
+	 * @param nom
+	 *            nombre del heroe
+	 * @param ini
+	 *            inicial del heroe
+	 */
 	public SuperHeroe(String nom, char ini) {
 		super(nom, ini);
 		ContenedorArmas = new Arbol<Arma>();
 	}
 
+	/**
+	 * Get del contenedor de armas del heroe
+	 * 
+	 * @return Arbol de armas
+	 */
 	public Arbol<Arma> getContenedorArmas() {
 		return ContenedorArmas;
 	}
 
+	/**
+	 * Inserta un arma en el contenedor del heroe
+	 * 
+	 * @param a
+	 *            arma a insertar
+	 */
 	public void insertarArmaHeroe(Arma a) {
 		ContenedorArmas.insertar(a);
 	}
 
+	/**
+	 * Devuelve el arma mas poderosa del contenedor
+	 * 
+	 * @return el arma con mayor poder
+	 */
 	public Arma mejorArma() {
 
 		return ContenedorArmas.mayor();
 
 	}
 
+	/**
+	 * Borra el arma del parametro del contenedor
+	 * 
+	 * @param a
+	 *            arma a borrar
+	 */
 	public void borrarArma(Arma a) {
 
 		ContenedorArmas.borrar(a);
 
 	}
 
+	/**
+	 * Set del contenedor de armas del heroe
+	 * 
+	 * @param contenedorArmas
+	 *            arbol de armas
+	 */
 	public void setContenedorArmas(Arbol<Arma> contenedorArmas) {
 		ContenedorArmas = contenedorArmas;
-	}
-
-	public static void main(String[] args) {
 	}
 
 }
