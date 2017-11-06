ad56be46bfc01eb516e3e75309b772814fd39248
truji <truji@truji-SATELLITE-PRO-C50-A-1HQ>
2017-11-06 18:44:45 +0100

    Clase villano comentada

--- ProyectoDP-Truji/src/personajes/Villano.java
+++ ProyectoDP-Truji/src/personajes/Villano.java
@@ -1,23 +1,49 @@
 package personajes;
 
+/**
+ * 
+ * ProyectoDP-Truji
+ * 
+ * @Fichero: Villano.java
+ * @Autor: David Trujillo Torres
+ * @Fecha: 6 nov. 2017
+ */
 public class Villano extends Personaje {
 	private Arma ArmaVillano;
 
+	/**
+	 * Constructor del villano que recibe su nombre, inicial y su arma como
+	 * parametros
+	 * 
+	 * @param nom
+	 *            nombre del villano
+	 * @param ini
+	 *            inicial del villano
+	 * @param a
+	 *            arma del villano
+	 */
 	public Villano(String nom, char ini, Arma a) {
 		super(nom, ini);
 		ArmaVillano = a;
 	}
 
+	/**
+	 * Get del arma del villano
+	 * 
+	 * @return arma que posee el villano
+	 */
 	public Arma getArmaVillano() {
 		return ArmaVillano;
 	}
 
+	/**
+	 * Set del arma del villano
+	 * 
+	 * @param armaVillano
+	 *            arma que poseera el villano
+	 */
 	public void setArmaVillano(Arma armaVillano) {
 		ArmaVillano = armaVillano;
-	}
-
-	public static void main(String[] args) {
-
 	}
 
 }
